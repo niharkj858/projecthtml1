9c27a87db2e865678d22f79735b49f5c06a0a131
Niharika Jain <niharikajain858@gmail.com>
2025-07-17 23:08:01 +0530

    Add files via upload

---
+++ oops.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class teacher
+{
+    //Properties or Attributes
+
+public:
+    string name;
+    string dept;
+    string subject;
+    double salary;
+
+    // Member Functions or Methods
+    void changedept(string newdept){
+        dept = newdept;
+    }
+
+};
+
+
+int main()
+{ //to create object
+  //classname.obj
+
+  teacher.t1;
+  t1.name="Niharika"
+  t1.subject="AI"
+  t1.dept="CSE"
+  t1.salary="1lakh"
+  teacher.t2;
+  teacher.t3;
+  teacher.t4;
+
+
+    return 0;
+}
